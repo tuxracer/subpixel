ef16034ae921fedce4e116eb9555769b7d4d0b5e
Derek Petersen <derek@stumbleupon.com>
2014-01-27 13:46:58 -0800

    Remove link from description

--- subpixel.user.js
+++ subpixel.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name          Subpixel
 // @namespace     https://github.com/tuxracer/subpixel
-// @description   Restore subpixel anti-aliasing http://www.usabilitypost.com/2012/11/05/stop-fixing-font-smoothing/
+// @description   Restore subpixel anti-aliasing
 // @downloadURL   https://raw.github.com/tuxracer/subpixel/master/subpixel.user.js
-// @version       1.0.0
+// @version       1.0.1
 // @match         http://*/*
 // @match         https://*/*
 // ==/UserScript==
